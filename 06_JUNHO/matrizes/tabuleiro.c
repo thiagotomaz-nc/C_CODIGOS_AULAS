0879af3f9240902b0f3280b29b3a7832228a4119
thiago tomaz <thiagotomaz.nc@gmail.com>
2025-06-27 06:55:14 -0300

    tabuleiro tabela_exibicao_controller

--- 06_JUNHO/matrizes/tabuleiro.c
+++ 06_JUNHO/matrizes/tabuleiro.c
@@ -2,15 +2,16 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define LINHAS 9
-#define COLUNAS 9
+#define LINHAS 10
+#define COLUNAS 10
 #define TRUE 1
 #define UNS 10
 
 int main(void)
 {
-    int tabuleiro[LINHAS][COLUNAS];
-    int acertos_jogadores[2] = {0, 0}, tentativas_jogadores[2] = {0, 0};
+    char tabuleiro_exibicao[LINHAS][COLUNAS];
+    int tabuleiro_controler[LINHAS][COLUNAS];
+    int acertos_jogadores[2] = {0, 0}, tentativas_jogadores[2] = {0, 0}, jogador = 1;
     int modo_jogo, menu_liberado = 0, linha_tabuleiro, coluna_tabuleiro;
     char detalhes_jogo;
 
@@ -76,18 +77,19 @@ int main(void)
     {
         for (int j = 0; j < COLUNAS; j++)
         {
-            tabuleiro[i][j] = 0;
+            tabuleiro_controler[i][j] = 0;
+            tabuleiro_exibicao[i][j] = '0';
         }
     }
 
     // Gerar os numeros 10  em posições aleatórias
-    for (int i = 0; i <= LINHAS; i++)
+    for (int i = 0; i < LINHAS; i++)
     {
-        int linhas_1 = rand() % 10;
-        int colunas_1 = rand() % 10;
-        if (!tabuleiro[linhas_1][colunas_1] == 1)
+        int linhas_1 = rand() % 11;
+        int colunas_1 = rand() % 11;
+        if (tabuleiro_controler[linhas_1][colunas_1] == 0)
         {
-            tabuleiro[linhas_1][colunas_1] = 1;
+            tabuleiro_controler[linhas_1][colunas_1] = 1;
         }
         else
         {
@@ -96,67 +98,119 @@ int main(void)
     }
 
     // Levando em consideração que os valores que irão sair do do_while serão apenas o 1 ou 2.
+
     if (modo_jogo == 1)
     {
         printf("\n********************************************");
         printf("\n\n >>> Modo Dois Jogadores (Sequencial)  selecionado\n");
-        printf("\n______________________________________________________________");
+        printf("\n__________________________________________________________________\n");
+        printf("\n-> Eh a vez do [Jogador 1] | RESUMO: %i acertos <-> %i tentativas", acertos_jogadores[0], tentativas_jogadores[0]);
+        printf("\nFACA SUA JOGADA\n\n");
+
+        printf("\n ========== TABULEIRO ========\n");
+
+        for (int i = 0; i < LINHAS; i++)
+        {
+            if (i == 0)
+            {
+                printf("  C  0 1 2 3 4 5 6 7 8 9\n");
+                printf("L    | | | | | | | | | |\n");
+            }
+            for (int j = 0; j <= COLUNAS; j++)
+            {
+                if (j == 0)
+                {
+                    printf("%i -> ", i);
+                }
+                else
+                {
+                    printf("%c ", tabuleiro_exibicao[i][j - 1]);
+                }
+            }
+            printf("\n");
+        }
+
         while (TRUE)
         {
-            printf("\n-> Eh a vez do [Jogador 1] | RESUMO: %i acertos <-> %i tentativas", acertos_jogadores[0], tentativas_jogadores[0]);
-            printf("\nFACA SUA JOGADA\n");
-            printf("\n----------------------------------------------------------------------------");
-            printf("\n ========== TABULEIRO ========\n");
-
-            for (int i = 0; i <= LINHAS; i++)
-            {
-                if (i == 0)
-                {
-                    printf("  C  0 1 2 3 4 5 6 7 8 9\n");
-                    printf("L    | | | | | | | | | |\n");
-                }
-                for (int j = 0; j <= COLUNAS + 1; j++)
-                {
-                    if (j == 0)
-                    {
-                        printf("%i -> ", i);
-                    }
-                    else
-                    {
-                        printf("%c ", '0');
-                    }
-
-                    // printf(" %i " , tabuleiro[i][j]);
-                }
-                printf("\n");
+            if (jogador==1){
+                
+            }else{
+                
             }
-
-            printf("\n");
-            printf("-> Digite a linha do tabuleiro: ");
+            printf("\n-> Digite a linha do tabuleiro: ");
             scanf("%i", &linha_tabuleiro);
             printf("-> Digite a coluna do tabuleiro: ");
             scanf("%i", &coluna_tabuleiro);
-            
-            if (tabuleiro[linha_tabuleiro][coluna_tabuleiro] == 1)
+
+            if (tabuleiro_controler[linha_tabuleiro][coluna_tabuleiro] == 1)
             {
-                 printf("\n********************************************************************");
-                printf("\n *** PARABENS VOCE ENCONTROU O NUMERO 1 ***,\nrestam %i numeros 1 (uns) para serem encontrados\n",UNS - acertos_jogadores[0]);
+                acertos_jogadores[0]++;
+                tentativas_jogadores[0]++;
+                tabuleiro_exibicao[linha_tabuleiro][coluna_tabuleiro] = '1';
+                printf("\n__________________________________________________________________\n");
+                printf("\n-> Eh a vez do [Jogador 1] | RESUMO: %i acertos <-> %i tentativas", acertos_jogadores[0], tentativas_jogadores[0]);
+                printf("\nFACA SUA JOGADA\n\n");
+                printf("\n********************************************************************");
+                printf("\n *** PARABENS VOCE ENCONTROU O NUMERO 1 ***,\nrestam %i numeros 1 (uns) para serem encontrados\n", LINHAS - acertos_jogadores[0]);
+                printf("\n-----------------------------------------------------\n");
                 printf("\n ========== TABULEIRO ========\n");
-                printf("\n");
+
                 for (int i = 0; i < LINHAS; i++)
                 {
-                    for (int j = 0; j < COLUNAS; j++)
+                    if (i == 0)
                     {
-                        printf(" %c ", '*');
-                        // printf(" %i " , tabuleiro[i][j]);
+                        printf("  C  0 1 2 3 4 5 6 7 8 9\n");
+                        printf("L    | | | | | | | | | |\n");
+                    }
+                    for (int j = 0; j <= COLUNAS; j++)
+                    {
+                        if (j == 0)
+                        {
+                            printf("%i -> ", i);
+                        }
+                        else
+                        {
+                            printf("%c ", tabuleiro_exibicao[i][j - 1]);
+                        }
                     }
                     printf("\n");
                 }
-                acertos_jogadores[0]++;
             }
             else
             {
-                printf("\n Error, tente novamente!!!");
+                tentativas_jogadores[0]++;
+                tabuleiro_exibicao[linha_tabuleiro][coluna_tabuleiro] = 'X';
+                printf("\n################################################################");
+                printf("\nNAO EXISTE 1 NESSA POSICAO  L [ %i ] <--> C [ %i ]  , tente novamente!!!", linha_tabuleiro, coluna_tabuleiro);
+                printf("\n__________________________________________________________________\n");
+                printf("\n-> Eh a vez do [Jogador 1] | RESUMO: %i acertos <-> %i tentativas", acertos_jogadores[0], tentativas_jogadores[0]);
+                printf("\nFACA SUA JOGADA\n\n");
+
+                printf("\n ========== TABULEIRO ========\n");
+
+                for (int i = 0; i < LINHAS; i++)
+                {
+                    if (i == 0)
+                    {
+                        printf("  C  0 1 2 3 4 5 6 7 8 9\n");
+                        printf("L    | | | | | | | | | |\n");
+                    }
+                    for (int j = 0; j <= COLUNAS; j++)
+                    {
+                        if (j == 0)
+                        {
+                            printf("%i -> ", i);
+                        }
+                        else
+                        {
+                            printf("%c ", tabuleiro_exibicao[i][j - 1]);
+                        }
+                    }
+
+                    printf("\n");
+                }
+
+                continue;
             }
         }
     }
@@ -164,91 +218,6 @@ int main(void)
     {
     }
 
-    /* printf("-> Gerando o tabuleiro para o jogador 1 \n");
-     for (int i = 0; i < LINHAS; i++)
-     {
-         for (int j = 0; j < COLUNAS; j++)
-         {
-             tabuleiro[i][j] = 0;
-         }
-     }
-     // Gerar os numeros 10  em posições aleatórias
-
-     for (int i = 0; i <= LINHAS; i++)
-     {
-         int linhas_1 = rand() % 10;
-         int colunas_1 = rand() % 10;
-         if (!tabuleiro[linhas_1][colunas_1] == 1)
-         {
-             tabuleiro[linhas_1][colunas_1] = 1;
-         }
-         else
-         {
-             i--;
-         }
-     }
-     printf("\n *** obrigado por esperar!!! ***");
-
-     printf("\n Modo 1 selecionado, o jogador 1 iniciara o jogo");
-     printf("\n");
-     printf("\n");
-     printf("*** JOGADOR 1: No tabuleiro abaixo existe 10 numeros uns escondidos, encontre-os usando a linha e a coluna ***");
-     printf("\n");
-     printf("\n");
-     printf("");
-     printf("\n ========== TABULEIRO ========\n");
-     printf("\n");
-     for (int i = 0; i < LINHAS; i++)
-     {
-         for (int j = 0; j < COLUNAS; j++)
-         {
-             printf(" %c ", '*');
-             // printf(" %i " , tabuleiro[i][j]);
-         }
-         printf("\n");
-     }
-
-     do
-     {
-
-         printf("\nscore");
-         printf("\nJOGADOR 1: %i acertos | %i tentativas", acertos_jogadores[0], tentativas_jogadores[0]);
-         printf("\n");
-         printf("-> Digite a linha do tabuleiro: ");
-         scanf("%i", &linha_tabuleiro);
-         printf("-> Digite a coluna do tabuleiro: ");
-         scanf("%i", &coluna_tabuleiro);
-
-         if (tabuleiro[linha_tabuleiro][coluna_tabuleiro] == 1)
-         {
-             printf("\n *** PARABENS VOCE ENCONTROU O NUMERO 1 ***, restam %i numeros 1 (uns) para serem encontrados\n", UNS - acertos_jogadores[0]);
-             printf("\n ========== TABULEIRO ========\n");
-             printf("\n");
-             for (int i = 0; i < LINHAS; i++)
-             {
-                 for (int j = 0; j < COLUNAS; j++)
-                 {
-                     printf(" %c ", '*');
-                     // printf(" %i " , tabuleiro[i][j]);
-                 }
-                 printf("\n");
-             }
-             acertos_jogadores[0]++;
-         }
-         else
-         {
-             printf("\n Error, tente novamente!!!");
-         }
-
-     } while (acertos_jogadores[0] = 10);
-
-     /*do{
-     printf("\n BEM-VINDO AO JOGO\n");
-     printf("\n %s escolha uma posição do tabuleiro (linha, coluna)\n", jogador01);
-     printf("\n");
-
-     }while(tentativas < 5);
- */
     printf("\n");
     return 0;
 }
