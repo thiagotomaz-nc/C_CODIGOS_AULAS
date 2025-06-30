50f0bbb8148e178d531baf3ad2e749b46e092cec
THIAGO TOMAZ DE ARAUJO <20251134040031@ifrn.local>
2025-06-30 17:37:18 -0300

    primeira estrutura

--- 06_JUNHO/estruturas/funcionarios.c
+++ 06_JUNHO/estruturas/funcionarios.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
+//definido pelo usuario
+typedef struct Funcionario
+{
+    int idade;
+    float salario;
+    int rg;
+};
+
+
 int main()
 {
     int n_funcionarios;
@@ -10,9 +19,7 @@ int main()
 
     if (n_funcionarios > 0)
     {
-        int idade_funcionario[n_funcionarios];
-        float salario_funcionario[n_funcionarios];
-        int rg_funcionario[n_funcionarios];
+        struct Funcionario funcionarios[n_funcionarios];
 
         printf("\n ***** CADASTRO DE FUNCIONARIOS ***** ");
 
@@ -20,31 +27,33 @@ int main()
         {
             printf("\n *** Digite o dados para o funcionario [%i] ***", i);
             printf("\n-> idade: ");
-            scanf("%i", &idade_funcionario[i]);
+            scanf("%i", &funcionarios[i].idade);
 
             printf("->Salario: ");
-            scanf("%f", &salario_funcionario[i]);
+            scanf("%f", &funcionarios[i].salario);
 
             printf("->RG: ");
-            scanf("%i", &rg_funcionario[i]);
+            scanf("%i", &funcionarios[i].rg);
 
             printf("\n");
         }
 
-        printf("\n ********* MEDIAS DOS FUNCIONARIOS ********* \n");
+        printf("\n ********* MEDIAS DOS FUNCIONARIOS ********* ");
 
         int soma_idade = 0;
         float soma_salario = 0.0;
 
         for (int i = 0; i < n_funcionarios; i++)
         {
-            soma_idade += idade_funcionario[i];
-            soma_salario += salario_funcionario[i];
+            soma_idade += funcionarios[i].idade;
+            soma_salario += funcionarios[i].salario;
         }
 
         printf("\n -> Media das idades: %.0lf", round(soma_idade / n_funcionarios));
         printf("\n -> Media Salarial: R$ %.2f", soma_salario / n_funcionarios);
     }
+
+    printf("\n");
 
     return 0;
 }
