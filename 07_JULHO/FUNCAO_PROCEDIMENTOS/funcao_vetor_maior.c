f2fd8f96b96ced64bc2c6befc73f90b396ee7a97
THIAGO TOMAZ DE ARAUJO <20251134040031@ifrn.local>
2025-07-08 16:05:04 -0300

    funcoes separadas

--- 07_JULHO/FUNCAO_PROCEDIMENTOS/funcao_vetor_maior.c
+++ 07_JULHO/FUNCAO_PROCEDIMENTOS/funcao_vetor_maior.c
@@ -10,97 +10,26 @@ int fatorial(int);
 #define N_INDICE 10
 
 // maior_valor(int * vetor); ponteiro apontando para o vetor
-int maior_valor(int [], int);
+int maior_valor(int[], int);
 
 int main(void)
 {
-    int numero;
-    float n1, n2;
-    int vetor[] = {10,32,5,6,11,1,20,3};
+    int vetor[] = {10, 32, 5, 6, 11, 1, 20, 3};
 
-    printf("\n O maior numero do vetor eh %i", maior_valor(vetor,8));
-    return 0;
-    
-    /*---------------------------------------*/
-
-    printf("\n-> Digite um numero: ");
-    scanf("%i", &numero);
-
-    printf(" %i! = %i\n\n", numero, fatorial(numero));
-
-    return 0;
-
-    /*--------------------------------------------------------------*/
-
-    printf("\n-> Digite 2 numeros: ");
-    scanf("%f %f", &n1, &n2);
-
-    int comparacao = compara_valores(n1, n2);
-    switch (comparacao)
-    {
-    case 1:
-        printf("%.2f > %.2f", n1, n2);
-        break;
-    case 0:
-        printf("%.2f = %.2f", n1, n2);
-        break;
-    case -1:
-        printf("%.2f < %.2f", n1, n2);
-        break;
-    default:
-        break;
-    }
-
+    printf("\n O maior numero do vetor eh %i", maior_valor(vetor, 8));
+    printf("\nBYTES: %i", sizeof(vetor) / sizeof(int));
+    /* TOTAL DE BYTES DO VETOR DIVIDIDO PELOS BYTES OCUPADOS POR UM INTEIRO (4)*/
     return 0;
 }
 
-int fatorial(int numero)
+int maior_valor(int vetor[], int tamanho)
 {
-    int fatorial = 1;
-    if (numero >= 0)
-    {
-        for (int i = 1; i <= numero; i++)
-        {
-            fatorial *= i;
-        }
+    int maior = vetor[0];
 
-        return fatorial;
-    }
-    else
+    for (int i = 1; i < tamanho; i++)
     {
-        printf("ERRO: Não existe Fatorial de numeros negativos!!!");
-        return -1;
-    }
-}
-
-// compara valor1 com o valor2
-// A ordem interfere na comparação
-float compara_valores(float valor1, float valor2)
-{
-    int valor = 0;
-    if (valor1 > valor2)
-    {
-        return 1;
-    }
-    else if (valor1 < valor2)
-    {
-        return -1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-/*float compara_valores(float valor1,float valor2){
-   return valor1 - valor2;
-}*/
-
-int maior_valor(int vetor[], int tamanho){
-    int maior=vetor[0];
-
-    for (int i =1; i<tamanho;i++){
-        if (maior < vetor[i]) maior=vetor[i];
+        if (maior < vetor[i])
+            maior = vetor[i];
     }
 
     return maior;
